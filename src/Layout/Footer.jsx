24e4b47420efa1e56172f47bacb5ad0fd300d16e
Pradheep Raj <pradheepselva111@gmail.com>
2024-07-08 09:51:54 +0530

    client scroll issue resolved

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
         </div>
       </section>
       <div className="flex justify-around items-start h-[80vh] bg-black relative">
-        <div class="footer-wave">
+        <div className="footer-wave">
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
@@ -31,7 +31,7 @@ const Footer = () => {
           >
             <path
               d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-              class="shape-fill"
+              className="shape-fill"
             ></path>
           </svg>
         </div>
@@ -65,9 +65,9 @@ const Footer = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3902.412079634939!2d79.84733470659349!3d12.015126492086406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a53659990ca3d51%3A0xec88dc0c7e9d240d!2sNEVAR%20Systems!5e0!3m2!1sen!2sin!4v1720356299611!5m2!1sen!2sin"
             width="450"
             height="350"
-            allowfullscreen=""
+            allowFullScreen=""
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
+            referrerPolicy="no-referrer-when-downgrade"
             className="rounded-xl"
           ></iframe>
           <div className="mt-5 flex justify-center items-center gap-5">
